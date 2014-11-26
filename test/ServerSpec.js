e2067b0148ef699705ef854d47db8fc0eace8857
sgenyk91 <sgenyk91@gmail.com>
2014-11-26 14:57:56 -0800

    tests passing

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -136,12 +136,12 @@ describe('', function() {
       it('Fetches the link url title', function (done) {
         requestWithSession(options, function(error, res, body) {
           db.knex('urls')
-            .where('title', '=', 'Rofl Zoo - Daily funny animal pictures')
+            .where('title', '=', 'Funny animal pictures, funny animals, funniest dogs')
             .then(function(urls) {
               if (urls['0'] && urls['0']['title']) {
                 var foundTitle = urls['0']['title'];
               }
-              expect(foundTitle).to.equal('Rofl Zoo - Daily funny animal pictures');
+              expect(foundTitle).to.equal('Funny animal pictures, funny animals, funniest dogs');
               done();
             });
         });
@@ -187,10 +187,10 @@ describe('', function() {
           'method': 'GET',
           'uri': 'http://127.0.0.1:4568/' + link.get('code')
         };
-
+        console.log(link.get('code'));
         requestWithSession(options, function(error, res, body) {
           var currentLocation = res.request.href;
-          expect(currentLocation).to.equal('http://www.roflzoo.com/');
+          expect(currentLocation).to.equal('http://roflzoo.com/');
           done();
         });
       });
